f560be38559ffd23ee29c71c50d29bca542ed0d7
Victor Decleire <vdecleir@fu-r10-p5.s19.be>
2024-02-12 17:56:50 +0100

    compatibilite m1

--- ft_printf.c
+++ ft_printf.c
@@ -6,33 +6,33 @@
 /*   By: vdecleir <vdecleir@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 14:28:52 by vdecleir          #+#    #+#             */
-/*   Updated: 2023/11/21 12:47:00 by vdecleir         ###   ########.fr       */
+/*   Updated: 2024/02/12 17:56:10 by vdecleir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	conversions(va_list ap, char c)
+static int	conversions(va_list *ap, char c)
 {
 	int	len;
 
 	len = 0;
 	if (c == 'c')
-		len = printf_putchar(va_arg(ap, int));
+		len = printf_putchar(va_arg(*ap, int));
 	else if (c == 's')
-		len = printf_putstr(va_arg(ap, char *));
+		len = printf_putstr(va_arg(*ap, char *));
 	else if (c == '%')
 		len = printf_putchar('%');
 	else if (c == 'd' || c == 'i')
-		len = count_signed(va_arg(ap, int));
+		len = count_signed(va_arg(*ap, int));
 	else if (c == 'u')
-		len = count_unsigned(va_arg(ap, unsigned int), "0123456789");
+		len = count_unsigned(va_arg(*ap, unsigned int), "0123456789");
 	else if (c == 'x')
-		len = count_unsigned(va_arg(ap, unsigned int), "0123456789abcdef");
+		len = count_unsigned(va_arg(*ap, unsigned int), "0123456789abcdef");
 	else if (c == 'X')
-		len = count_unsigned(va_arg(ap, unsigned int), "0123456789ABCDEF");
+		len = count_unsigned(va_arg(*ap, unsigned int), "0123456789ABCDEF");
 	else if (c == 'p')
-		len = count_adress(va_arg(ap, void const *), "0123456789abcdef");
+		len = count_adress(va_arg(*ap, void const *), "0123456789abcdef");
 	else
 		len = printf_putchar(c);
 	return (len);
@@ -53,7 +53,7 @@ int	ft_printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			count += conversions(ap, format[i + 1]);
+			count += conversions(&ap, format[i + 1]);
 			i++;
 		}
 		else
@@ -63,12 +63,3 @@ int	ft_printf(const char *format, ...)
 	va_end(ap);
 	return (count);
 }
-
-/* int	main()
-{
-	char *poc = "hey";
-	int i = ft_printf("Salut j'ai %i %% a mon %s soit %x ou %X en hexa %p\n", 14, "test", -125, 125, poc);
-	//int	j = printf("Salut j'ai %i %% a mon %s soit %x ou %X en hexa %p\n", 14, "test", -125, 125, poc);
-	ft_printf("%i\n", i);
-	//printf("%i\n", j);
-} */
